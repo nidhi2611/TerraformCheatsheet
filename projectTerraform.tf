98cadca44b35276a6759690ce0764f97c6a64d4c
Nidhi Gupta <rivanna2512@gmail.com>
2024-02-13 14:55:23 +0530

    Update projectTerraform.tf with the web-server-instance

--- projectTerraform.tf
+++ projectTerraform.tf
@@ -107,6 +107,28 @@ resource "aws_network_interface" "networkInterface" {
 resource "aws_eip" "ip" {
   vpc                    = "true"
   network_interface         = aws_network_interface.networkInterface.id
-  associate_with_private_ip = "10.0.0.10"
-  depends_on = [  ]
+  associate_with_private_ip = "10.0.0.50"
+  depends_on = [aws_internet_gateway.gateway1]
+}
+
+resource "aws_instance" "web-server-instance" {
+  ami = "ami-01e82af4e524a0aa3"
+  instance_type = "t2.micro"
+  availability_zone = "us-west-2a"
+
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.networkInterface.id
+  }
+
+  user_data = <<-EOF
+        #!/bin/bash
+        sudo apt update -y
+        sudo apt install apache2 -y
+        sudo systemctl start apache2
+        sudo bash -c 'echo yor very first web server > /var/www/html/index.html'
+        EOF
+  tags = {
+    Name = "web-server-instance"
+  }
 }
